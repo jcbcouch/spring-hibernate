165525a58a6d792290b275df7c1206833e2fc0a8
jcbcouch <jcbcouch@gmail.com>
2023-10-10 23:50:11 -0500

    init

---
+++ src/main/java/com/jacobcouch/springhibernateapp/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.jacobcouch.springhibernateapp.dao;
+
+import com.jacobcouch.springhibernateapp.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
---
+++ src/test/java/com/jacobcouch/springhibernateapp/SpringHibernateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobcouch.springhibernateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
