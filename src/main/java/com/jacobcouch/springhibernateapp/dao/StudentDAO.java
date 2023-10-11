b674ba190f59a0ec7ac27bbb68480c2d0f32d96c
jcbcouch <jcbcouch@gmail.com>
2023-10-11 18:21:08 -0500

    student crud

--- src/main/java/com/jacobcouch/springhibernateapp/dao/StudentDAO.java
+++ src/main/java/com/jacobcouch/springhibernateapp/dao/StudentDAO.java
@@ -2,6 +2,19 @@ package com.jacobcouch.springhibernateapp.dao;
 
 import com.jacobcouch.springhibernateapp.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
 }
